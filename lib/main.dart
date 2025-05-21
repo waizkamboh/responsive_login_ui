0af7098d25c72ac94d7209b0b4149d0b69f41f9d
waizkamboh <waizkamboh6@gmail.com>
2025-05-21 12:50:07 +0500

    set dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_login_ui/login_screen.dart';
+import 'package:responsive_login_ui/pallete.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData.dark().copyWith(
+         scaffoldBackgroundColor: Pallete.backgroundColor,
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginScreen(),
     );
   }
 }
